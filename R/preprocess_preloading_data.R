23cca1d7b4741406a5808c4cc90b6c0528c1ddd3
Anthony Twesigye <twesigye10@gmail.com>
2021-10-15 08:31:46 +0300

    some code cleaning and adding comments

--- R/preprocess_preloading_data.R
+++ R/preprocess_preloading_data.R
@@ -1,8 +1,7 @@
 library(tidyverse)
 
+# load the data for aggregation
 df_preloading_data <- read_csv("inputs/sample_data_for_hh_visit.csv")
-
-# "Registration_Group", "IndividualID", "Settlement", "Zone", "Village", "Relationship", "Sex", "Age" 
 
 # determine hh size
 df_hh_size <- df_preloading_data %>% 
@@ -33,7 +32,7 @@ df_hh_with_age_atleast_12 <- df_preloading_data %>%
   group_by(Registration_Group) %>%
   summarise(hh_age_atleast_12 = n())
 
-
+# join the aggregated fields to household data
 df_data_with_extra_columns <- df_preloading_data %>% 
   distinct(Registration_Group, Settlement, Zone, Village) %>% 
   left_join(df_hh_size, by = "Registration_Group") %>% 
@@ -47,21 +46,5 @@ df_data_with_extra_columns <- df_preloading_data %>%
          hh_age_atleast_12 = ifelse(is.na(hh_age_atleast_12), 0, hh_age_atleast_12)
          )
 
-
+# write the result to a file
 write_csv(x = df_data_with_extra_columns, file = "outputs/hh_level_data.csv")
-
-
-# 
-# df_data_with_age_12_13 <- df_preloading_data %>% 
-#   distinct(Registration_Group, Settlement, Zone, Village) %>% 
-#   left_join(df_hh_with_age_6_12, by = "Registration_Group") %>% 
-#   mutate(hh_age_6_12 = ifelse(is.na(hh_age_6_12), 0, hh_age_6_12))
-# 
-# 
-# 
-# 
-# df_data_with_age_13_16 <- df_preloading_data %>% 
-#   distinct(Registration_Group, Settlement, Zone, Village) %>% 
-#   left_join(df_hh_with_age_13_16, by = "Registration_Group") %>% 
-#   mutate(hh_age_13_16 = ifelse(is.na(hh_age_13_16), 0, hh_age_13_16))
-
