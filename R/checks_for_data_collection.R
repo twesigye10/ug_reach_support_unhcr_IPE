e06c267a340166d930e3e230a10b6b06e60217c2
Anthony Twesigye <twesigye10@gmail.com>
2022-01-06 10:52:21 +0300

    loading data

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -1,4 +1,16 @@
 library(tidyverse)
 library(lubridate)
 library(glue)
+
 source("R/checks_for_other_responses.R")
+
+# read data 
+df_tool_data <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Questionnaire_for_Sampled_Households.xlsx") %>% 
+  mutate(i.check.uuid = `_uuid`,
+         i.check.start_date = as_date(start),
+         start = as_datetime(start),
+         end = as_datetime(end))
+
+df_survey <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "survey")
+df_choices <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "choices")
+
