d91204a20790eea88cf515560fe641e4e78c1aef
Anthony Twesigye <twesigye10@gmail.com>
2022-01-06 11:31:27 +0300

    duration of the survey

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -17,3 +17,40 @@ df_choices <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx
 # output holder -----------------------------------------------------------
 
 checks_output <- list()
+
+# time checks -------------------------------------------------------------
+
+# duration of the survey
+min_time_of_survey <- 20
+max_time_of_survey <- 120
+
+df_c_survey_time <-  df_tool_data %>% 
+  mutate(int.survey_time_interval = lubridate::time_length(end - start, unit = "min"),
+         int.survey_time_interval = ceiling(int.survey_time_interval),
+         
+         i.check.type = "remove_survey",
+         i.check.name = "point_number",
+         i.check.current_value = "",
+         i.check.value = "",
+         i.check.issue_id = case_when(
+           int.survey_time_interval < min_time_of_survey ~ "less_survey_time",
+           int.survey_time_interval > max_time_of_survey ~ "more_survey_time",
+           TRUE ~ "normal_survey_time" ),
+         i.check.issue = glue("{int.survey_time_interval} min taken to do the survey"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.uuid_cl = paste0(i.check.uuid, "_", i.check.type, "_", i.check.name),
+         i.check.so_sm_choices = "")%>% 
+  filter(i.check.issue_id %in% c("less_survey_time", "more_survey_time")) %>% 
+  dplyr::select(starts_with("i.check"))%>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+if(exists("df_c_survey_time")){
+  if(nrow(df_c_survey_time) > 0){
+    checks_output$df_c_survey_time <- df_c_survey_time
+  }
+}
