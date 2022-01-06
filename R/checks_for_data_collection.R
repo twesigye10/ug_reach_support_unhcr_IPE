02a79b4f99b38ab01b0e681f9254149a0c15d7ab
Anthony Twesigye <twesigye10@gmail.com>
2022-01-06 13:21:06 +0300

    availability of gps coordinates

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -8,6 +8,7 @@ source("R/checks_for_other_responses.R")
 df_tool_data <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Questionnaire_for_Sampled_Households.xlsx") %>% 
   mutate(i.check.uuid = `_uuid`,
          i.check.start_date = as_date(start),
+         i.check.settlement = settlement,
          start = as_datetime(start),
          end = as_datetime(end))
 
@@ -18,11 +19,11 @@ df_choices <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx
 
 checks_output <- list()
 
-# time checks -------------------------------------------------------------
 
-# duration of the survey
+# duration of the survey --------------------------------------------------
+
 min_time_of_survey <- 20
-max_time_of_survey <- 120
+max_time_of_survey <- 180
 
 df_c_survey_time <-  df_tool_data %>% 
   mutate(int.survey_time_interval = lubridate::time_length(end - start, unit = "min"),
@@ -54,3 +55,35 @@ if(exists("df_c_survey_time")){
     checks_output$df_c_survey_time <- df_c_survey_time
   }
 }
+
+
+# availability of gps coordinates -----------------------------------------
+
+df_c_survey_gps <-  df_tool_data %>% 
+  filter(is.na(gps_coordinates)) %>% 
+  mutate(int.survey_time_interval = lubridate::time_length(end - start, unit = "min"),
+         int.survey_time_interval = ceiling(int.survey_time_interval),
+         
+         i.check.type = "remove_survey",
+         i.check.name = "point_number",
+         i.check.current_value = "",
+         i.check.value = "",
+         i.check.issue_id = "no_gps_coordinates",
+         i.check.issue = glue("no gps coordinates"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.uuid_cl = paste0(i.check.uuid, "_", i.check.type, "_", i.check.name),
+         i.check.so_sm_choices = "")%>% 
+  dplyr::select(starts_with("i.check"))%>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+if(exists("df_c_survey_gps")){
+  if(nrow(df_c_survey_gps) > 0){
+    checks_output$df_c_survey_gps <- df_c_survey_gps
+  }
+}
+
