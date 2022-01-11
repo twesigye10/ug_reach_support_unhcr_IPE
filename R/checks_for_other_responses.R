24cf7518281210841f10c3b0c7763ca792d99641
Anthony Twesigye <twesigye10@gmail.com>
2022-01-11 16:18:59 +0300

    improving the other regex search

--- R/checks_for_other_responses.R
+++ R/checks_for_other_responses.R
@@ -24,7 +24,9 @@ extract_other_data <- function(input_tool_data, input_survey, input_choices) {
       select(uuid, start_date, settlement, other_text = cln, current_value = current_parent_qn) %>% 
       filter(!is.na(other_text), !other_text %in% c(" ", "NA")) %>% 
       mutate( other_name = cln, 
-              int.my_current_val_extract = ifelse(str_detect(current_value, "other\\b"), str_extract_all(string = current_value, pattern = "other\\b|[a-z]+._other\\b"), current_value),
+              int.my_current_val_extract = ifelse(str_detect(current_value, "\\bother\\b"), 
+                                                  str_extract_all(string = current_value, pattern = "\\bother\\b|\\w+_other\\b"), 
+                                                  current_value),
               value = "",
               parent_qn = current_parent_qn)
     df_other_response_data <- rbind(df_other_response_data, df_filtered_data)
