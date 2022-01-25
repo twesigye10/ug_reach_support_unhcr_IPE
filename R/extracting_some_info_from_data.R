e0254cf3ef75742338218116be1efcf2f9290c3f
Anthony Twesigye <twesigye10@gmail.com>
2022-01-25 12:31:39 +0300

    finding average survey time

--- R/extracting_some_info_from_data.R
+++ R/extracting_some_info_from_data.R
@@ -10,5 +10,21 @@ df_tool_data <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Questio
          start = as_datetime(start),
          end = as_datetime(end))
 
+# log
+df_log <- readr::read_csv("inputs/combined_checks_IPE_questionnaire_for_sampled_households.xlsx")
+
+# tool
 df_survey <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "survey")
 df_choices <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "choices")
+
+# average survey time
+
+average_survey_time <- df_tool_data %>% 
+  mutate(int.survey_time_interval = lubridate::time_length(end - start, unit = "min"),
+         int.survey_time_interval = ceiling(int.survey_time_interval)
+         )%>% 
+  select(int.survey_time_interval) %>% 
+  summarise(average_time = mean(int.survey_time_interval, na.rm = TRUE))
+  
+
+
