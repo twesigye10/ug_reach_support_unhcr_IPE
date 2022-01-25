d34d966ad1251edd3820dada51249a1f3f27f334
Anthony Twesigye <twesigye10@gmail.com>
2022-01-25 12:23:21 +0300

    read data and survey

--- R/extracting_some_info_from_data.R
+++ R/extracting_some_info_from_data.R
@@ -1,3 +1,14 @@
 library(tidyverse)
 library(lubridate)
 library(glue)
+
+# read data 
+df_tool_data <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Questionnaire_for_Sampled_Households.xlsx") %>% 
+  mutate(i.check.uuid = `_uuid`,
+         i.check.start_date = as_date(start),
+         i.check.settlement = settlement,
+         start = as_datetime(start),
+         end = as_datetime(end))
+
+df_survey <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "survey")
+df_choices <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "choices")
