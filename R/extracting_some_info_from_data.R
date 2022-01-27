f364b9367b958fa2523bd5a0555e2487fbaeffe1
Anthony Twesigye <twesigye10@gmail.com>
2022-01-27 18:42:43 +0300

    number of surveys with comment "option already exists"

--- R/extracting_some_info_from_data.R
+++ R/extracting_some_info_from_data.R
@@ -11,7 +11,7 @@ df_tool_data <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Questio
          end = as_datetime(end))
 
 # log
-df_log <- readr::read_csv("inputs/combined_checks_IPE_questionnaire_for_sampled_households.xlsx")
+df_log <- readxl::read_excel("inputs/combined_checks_IPE_questionnaire_for_sampled_households.xlsx")
 
 # tool
 df_survey <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "survey")
@@ -27,4 +27,35 @@ average_survey_time <- df_tool_data %>%
   summarise(average_time = mean(int.survey_time_interval, na.rm = TRUE))
   
 
+# summary from the logs reviewed ------------------------------------------
 
+# number of surveys with comment "option already exists"
+
+df_log %>% 
+  filter(str_detect(string = comment, pattern = fixed('already exist', ignore_case = TRUE))) %>% 
+  group_by(uuid) %>% 
+  select(uuid) %>% 
+  unique()
+
+# most appearing questions
+# df_survey_modified <- df_survey %>% 
+#   mutate(qn_type = str_extract(string = type, pattern = "^\\w+\\b")) %>%  
+#   select(qn_type , name)
+
+df_log %>% 
+  filter(!is.na(name),  !type %in% c("add_option")) %>% 
+  # left_join(df_survey_modified, by = "name") %>% 
+  group_by(name, label) %>% 
+  summarise(number_of_occurances = n()) %>% 
+  filter(number_of_occurances > 5) %>% 
+  arrange(desc(number_of_occurances))
+  
+# reapeated similar text
+df_log %>% 
+  filter(!is.na(name), !name %in% c("gps_coordinates"), !type %in% c("add_option")) %>% 
+  mutate(other_text = str_to_lower(other_text),
+         other_text = str_squish(other_text)) %>% 
+  group_by(name, label, other_text) %>% 
+  summarise(number_of_occurances = n()) %>% 
+  filter(number_of_occurances > 1) %>% 
+  arrange(desc(number_of_occurances))
