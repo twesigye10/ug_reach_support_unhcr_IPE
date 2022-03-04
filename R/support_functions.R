c8f259156ac098ae38555b06da08ec2d786f2542
Anthony Twesigye <twesigye10@gmail.com>
2022-03-04 12:44:03 +0300

    Outliers

--- R/support_functions.R
+++ R/support_functions.R
@@ -44,6 +44,31 @@ check_duplicates_by_uuid <- function(input_tool_data) {
     dplyr::select(starts_with("i.check"))%>% 
     rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 }
+
+# Outliers ----------------------------------------------------------------
+
+check_outliers <- function(input_tool_data, input_column, input_lower_limit, input_upper_limit) {
+  input_tool_data %>% 
+    filter(!!sym(input_column) < input_lower_limit | !!sym(input_column) > input_upper_limit) %>% 
+    mutate(i.check.type = "remove_survey",
+           i.check.name = input_column,
+           i.check.current_value = as.character(!!sym({{input_column}})),
+           i.check.value = "",
+           i.check.issue_id = "logic_c_outlier",
+           i.check.issue = paste(input_column,": ",!!sym({{input_column}}), "seems to be an outlier, needs engagement with enumerator"),
+           i.check.other_text = "",
+           i.check.checked_by = "",
+           i.check.checked_date = as_date(today()),
+           i.check.comment = "",
+           i.check.reviewed = "",
+           i.check.adjust_log = "",
+           i.check.uuid_cl = paste0(i.check.uuid, "_", i.check.type, "_", i.check.name),
+           i.check.so_sm_choices = "") %>%
+    ungroup() %>%
+    dplyr::select(starts_with("i.check"))%>%
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
+
 # function: extract others checks  ----------------------------------------
 
 extract_other_data <- function(input_tool_data, input_survey, input_choices) {
