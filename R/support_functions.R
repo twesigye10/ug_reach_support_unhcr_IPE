0369c07b99ca9863715f6eeb8fa0220d15904000
Anthony Twesigye <twesigye10@gmail.com>
2022-03-28 22:13:46 +0300

    add checks data to a list of checks

--- R/support_functions.R
+++ R/support_functions.R
@@ -195,3 +195,21 @@ extract_other_data <- function(input_tool_data, input_survey, input_choices) {
            so_sm_choices)
 }
 
+# add checks data to a list of checks  ------------------------------------
+
+add_checks_data_to_list <- function(input_list_name, input_df_name) {
+  if(exists(input_list_name) & exists(input_df_name)){
+    # get the current values of these objects
+    global_list_data <- get(input_list_name, envir = .GlobalEnv)
+    global_df_data <- get(input_df_name, envir = .GlobalEnv)
+    # check if the dataframe of interest has data
+    if(nrow(global_df_data) > 0){
+      # append the data frame to the list
+      global_list_data[[input_df_name]] <-  global_df_data
+      # assign the data to the global environment
+      assign(x = input_list_name, value = global_list_data, envir = .GlobalEnv)
+    }
+  } else{
+    message("given objects not in the global environment.")
+  }
+}
