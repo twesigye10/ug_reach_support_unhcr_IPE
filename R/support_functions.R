286f7762ccd3980ced18aad554d0bee7229746b4
Anthony Twesigye <twesigye10@gmail.com>
2022-05-27 12:30:36 +0300

    consider only surveys with positive time interval

--- R/support_functions.R
+++ R/support_functions.R
@@ -77,11 +77,12 @@ get_average_survey_time <- function(input_tool_data) {
     mutate(int.survey_time_interval = lubridate::time_length(end - start, unit = "min"),
            int.survey_time_interval = ceiling(int.survey_time_interval)
     )%>% 
+    filter(int.survey_time_interval > 0 ) %>% # consider only surveys with positive time interval
     select(int.survey_time_interval)
     
   # lower and upper quantiles of survey duration
-  lower_limit = quantile(df_tool_data_with_time_interval$int.survey_time_interval, 0.01, na.rm =TRUE)
-  upper_limit = quantile(df_tool_data_with_time_interval$int.survey_time_interval, 0.99, na.rm =TRUE)
+  lower_limit = quantile(df_tool_data_with_time_interval$int.survey_time_interval, 0.025, na.rm =TRUE)
+  upper_limit = quantile(df_tool_data_with_time_interval$int.survey_time_interval, 0.975, na.rm =TRUE)
   
   df_tool_data_with_time_interval %>% 
     filter(int.survey_time_interval > lower_limit | int.survey_time_interval < upper_limit) %>% 
