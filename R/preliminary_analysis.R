f3b8142f0b83a1ee429ce6fb37a8d4a45c6eba91
Anthony Twesigye <twesigye10@gmail.com>
2023-04-19 20:34:04 +0300

    analysis

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -20,3 +20,34 @@ df_tool_data_support <- df_survey |>
 
 # dap
 dap <- read_csv("inputs/r_dap_ipe_hh_sampled.csv")
+
+# set up design object
+ref_svy <- as_survey(.data = df_main_clean_data)
+
+# analysis
+
+df_main_analysis <- analysis_after_survey_creation(input_svy_obj = ref_svy,
+                                                   input_dap = dap)
+# merge analysis
+
+combined_analysis <- df_main_analysis
+
+full_analysis_long <- combined_analysis |> 
+  mutate(variable = ifelse(is.na(variable) | variable %in% c(""), variable_val, variable),
+         int.variable = ifelse(str_detect(string = variable, pattern = "^i\\."), str_replace(string = variable, pattern = "^i\\.", replacement = ""), variable)) |> 
+  left_join(df_tool_data_support, by = c("int.variable" = "name")) |> 
+  relocate(label, .after = variable) |> 
+  mutate(label = ifelse(is.na(label), variable, label),
+         `mean/pct` = ifelse(select_type %in% c("integer") & !str_detect(string = variable, pattern = "^i\\."), `mean/pct`, `mean/pct`*100),
+         `mean/pct` = round(`mean/pct`, digits = 2)) |> 
+  select(`Question`= label, 
+         variable, 
+         `choices/options` = variable_val, 
+         `Results(mean/percentage)` = `mean/pct`, 
+         n_unweighted, 
+         population, 
+         subset_1_name, 
+         subset_1_val)
+
+# output analysis
+write_csv(full_analysis_long, paste0("outputs/", butteR::date_file_prefix(), "_full_analysis_lf_ipe_hh_sampled.csv"), na="")
