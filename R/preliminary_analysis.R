12e1a0b58ae0c44fb705e613fe56f27c4f4b489b
Anthony Twesigye <twesigye10@gmail.com>
2023-04-19 20:32:21 +0300

    tool and dap

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -9,3 +9,14 @@ data_nms <- names(readxl::read_excel(path = data_path, n_max = 2000, sheet = "cl
 c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "guess")
 
 df_main_clean_data <- readxl::read_excel(path = data_path, sheet = "cleaned_data", col_types = c_types, na = "NA")
+
+# tool
+df_survey <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "survey") 
+
+df_tool_data_support <- df_survey |> 
+  select(type, name, label = `label::english`) |> 
+  filter(str_detect(string = type, pattern = "integer|date|select_one|select_multiple")) |> 
+  separate(col = type, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop" )
+
+# dap
+dap <- read_csv("inputs/r_dap_ipe_hh_sampled.csv")
