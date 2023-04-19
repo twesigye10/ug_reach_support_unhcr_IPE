febd81b92990addaacf9909c3e09cbb90b5dc8a2
Anthony Twesigye <twesigye10@gmail.com>
2023-04-19 20:26:57 +0300

    clean data

---
+++ R/preliminary_analysis.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(srvyr)
+library(supporteR)
+
+# clean data
+data_path <- "inputs/clean_data_ipe_hh_sampled.xlsx"
+
+data_nms <- names(readxl::read_excel(path = data_path, n_max = 2000, sheet = "cleaned_data"))
+c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "guess")
+
+df_main_clean_data <- readxl::read_excel(path = data_path, sheet = "cleaned_data", col_types = c_types, na = "NA")
