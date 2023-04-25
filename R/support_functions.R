92cd6783ba6bfabefd8134cac6aa749e0af6ee2c
Anthony Twesigye <twesigye10@gmail.com>
2023-04-25 09:48:49 +0300

    function now in supporteR

--- R/support_functions.R
+++ R/support_functions.R
@@ -18,31 +18,6 @@ create_dap_skeleton <- function(input_tool_name, input_ignore_columns) {
     write_csv(file = paste0("outputs/r_dap_", str_replace(string = input_tool_name, pattern = fixed(pattern = "_tool", ignore_case = TRUE), replacement = ""), ".csv"), na = "NA")
 }
 
-# duplicate uuid ----------------------------------------------------------
-
-# check uuids of surveys for duplicates
-check_duplicates_by_uuid <- function(input_tool_data) {
-  input_tool_data %>% 
-    group_by(i.check.uuid) %>% 
-    mutate(rank = row_number()) %>% 
-    filter(rank > 1) %>%  
-    mutate(
-      i.check.type = "remove_survey",
-      i.check.name = "point_number",
-      i.check.current_value = "",
-      i.check.value = "",
-      i.check.issue_id = "duplicate_uuid",
-      i.check.issue = "The uuid: {i.check.uuid} is duplicate in the data",
-      i.check.other_text = "",
-      i.check.checked_by = "",
-      i.check.checked_date = as_date(today()),
-      i.check.comment = "", 
-      i.check.reviewed = "",
-      i.check.adjust_log = "",
-      i.check.so_sm_choices = "")%>% 
-    dplyr::select(starts_with("i.check"))%>% 
-    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
-}
 
 # Outliers ----------------------------------------------------------------
 
