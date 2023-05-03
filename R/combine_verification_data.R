f661007207faab8ba4962097104ba572567aec2c
Anthony Twesigye <twesigye10@gmail.com>
2023-05-03 12:18:18 +0300

    creating disability indicator

--- R/combine_verification_data.R
+++ R/combine_verification_data.R
@@ -17,5 +17,47 @@ df_combined_verif_data <- purrr::map_dfr(.x = df_ipe_verif_data_list,
   mutate(batch_name = str_replace(string = batch_name, pattern = "inputs/REACH DataPWD/", replacement = ""),
          batch_name = str_replace(string = batch_name, pattern = "\\.csv$", replacement = ""))
 
-rio::export(df_combined_verif_data, file = paste0("outputs/", butteR::date_file_prefix(), "_combined_ipe_verif_data_with_batch_name.xlsx"))
+# rio::export(df_combined_verif_data, file = paste0("outputs/", butteR::date_file_prefix(), "_combined_ipe_verif_data_with_batch_name.xlsx"))
 
+# writexl::write_xlsx(x = df_combined_verif_data, path = paste0("outputs/", butteR::date_file_prefix(), "_combined_ipe_verif_data_with_batch_name.xlsx"))
+
+readr::write_csv(x = df_combined_verif_data, file = paste0("outputs/", butteR::date_file_prefix(), "_combined_ipe_verif_data_with_batch_name.csv"))
+
+
+
+# extract required vars for access to services ----------------------------
+
+df_combined_verif_data |> 
+  mutate(i.coping_less_expenditure = "64",
+         i.primary_needs_top3 = "92",
+         # i.fhhh_not_attending = gender_hoh sum_not_attending_enrolled,
+         # i.mhhh_not_attending = gender_hoh sum_not_attending_enrolled,
+         # i. = why_child_not_regularly_attending,
+         # i. = hh_member_skills_training,
+         # i. = ,
+         i.child_work_1217 = "123",
+         # i. = withdrew_children_from_school,
+         i.coping_withdraw_school = "83",
+         i.coping_withdraw_school_fhhh = "83",
+         i.coping_withdraw_school_mhhh = "83",
+         i.indv_medical_illness_3months = "31",
+         i.indv_medical_illness_3months_sought_asst = "32",
+         i.indv_difficulties_control_emotions = "30",
+         int.difficulty_walking = as.character(14),
+         int.difficulty_lifting = as.character(15),
+         int.difficulty_selfcare = as.character(16),
+         int.difficulty_seeing = as.character(26),
+         int.difficulty_hearing = as.character(27),
+         int.difficulty_communicating = as.character(29),
+         int.difficulty_remembering = as.character(28),
+         int.difficulty_emotions = as.character(30),
+         int.disability = paste(int.difficulty_walking, int.difficulty_lifting, int.difficulty_selfcare, int.difficulty_seeing, int.difficulty_hearing, int.difficulty_communicating),
+         i.wgss_3 = ifelse(str_detect(string = int.disability, pattern = "1706|1707"), "yes_disability", "no_disability"),
+         i.hohh_wgss_3 = case_when(i.wgss_3 & progres_relationshiptofpname %in% c("Focal Point") ~ "disability_focal_point",
+                                   i.wgss_3 & !progres_relationshiptofpname %in% c("Focal Point") ~ "disability_other")
+  ) |> 
+  select(AnonymizedGrp, progres_maritalstatusname, progres_age,
+         progres_relationshiptofpname, progres_coalocationlevel1name, progres_coalocationlevel2name,
+         progres_countryoforiginidname, progres_sexname,
+         starts_with("i.|int.")) |> 
+  write_csv("inputs/REACH DataPWD/combined_access_to_services_extract_ipe_verif_data.csv")
