e79d32e1f8b2ff83ef46f89383c0e67e3ae01217
Anthony Twesigye <twesigye10@gmail.com>
2023-05-05 11:02:26 +0300

    matching data with provided ids

--- R/match_sampled_and_verification_data.R
+++ R/match_sampled_and_verification_data.R
@@ -17,6 +17,11 @@ ipe_data_adjumani <- "inputs/REACH DataPWD/Adjumani IPE 06042023.csv"
 df_ipe_data_adjumani <- readr::read_delim(file = ipe_data_adjumani, delim = ";")
 colnames(df_ipe_data_adjumani)
 
+data_path_verification <- "outputs/combined_ipe_verif_data_with_batch_name.csv"
+
+df_ipe_data_complete_verification <- readr::read_csv(file =  data_path_verification)
+
+  
 # sampled data ------------------------------------------------------------
 
 sampled_data_loc <- "inputs/Reach Household visit/IPE_questionnaire_for_sampled_households_sqlite.xlsx"
@@ -30,13 +35,34 @@ df_hh_sampled_data <- readxl::read_excel(sampled_data_loc)
 
 # check that the key works for both datasets ------------------------------
 
-df_hh_sampled_data_id <- df_hh_sampled_data |> select(GroupAnonymized)
+# df_hh_sampled_data_id <- df_hh_sampled_data |> select(GroupAnonymized)
+# 
+# df_data_delim_id <- df_data_delim |> select(AnonymizedGrp) |> pull()
+# 
+# df_hh_sampled_data |> filter(GroupAnonymized %in% df_data_delim$AnonymizedGrp) |> nrow()
+# 
+# df_hh_sampled_data_id |> filter(GroupAnonymized %in% df_data_delim_id)
 
-df_data_delim_id <- df_data_delim |> select(AnonymizedGrp) |> pull()
+# sampled data matching
 
-df_hh_sampled_data |> filter(GroupAnonymized %in% df_data_delim$AnonymizedGrp) |> nrow()
+df_id_mappings <- readr::read_csv("inputs/REACH DataPWD/RegistrationGroupDataMappingKeys.csv")
+
+df_sampled_matching <- df_hh_sampled_data |> 
+  filter(GroupAnonymized %in% df_id_mappings$GroupAnonymized)
+
+# nrow(df_hh_sampled_data) ##: 23835
+
+nrow(df_sampled_matching) ## : 16115
+
+# deficit: 7720
+# deficit
+df_sampled_not_matching <- df_hh_sampled_data |> 
+  filter(!GroupAnonymized %in% df_id_mappings$GroupAnonymized)
+nrow(df_sampled_not_matching)
+df_sampled_not_matching |> select(GroupAnonymized) |> 
+  write_csv("outputs/un_matching_GroupAnonymized_sampled_data.csv")
 
 
-
-df_hh_sampled_data_id |> filter(GroupAnonymized %in% df_data_delim_id)
-
+# verification data matching
+df_verification_matching <- df_ipe_data_complete_verification |> 
+  filter(AnonymizedGrp %in% df_id_mappings$AnonymizedGrp)
