a07330c6618686b151398a6ccab416f29c56edca
Anthony Twesigye <twesigye10@gmail.com>
2023-05-08 14:06:01 +0300

    verification data analysis sev

---
+++ R/preliminary_analysis_verification_sev.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+library(srvyr)
+library(supporteR)
+
+# source("R/composite_indicators.R")
+source("R/make_weights.R")
+
+# clean data
+data_path <- "inputs/REACH DataPWD/combined_ipe_verif_data_with_batch_name.csv"
+
+df_main_clean_data <- readr::read_csv(file =  data_path)
+
+# # question/choices codes and labels
+df_choices <- readxl::read_excel("inputs/REACH DataPWD/Questions and Responses CODES.xlsx", sheet = "original") |> 
+  mutate(choice_code = as.character(AnswerID),
+         choice_label = as.character(Answer)) |> 
+  select(choice_code, choice_label)
+
+qn_label_lookup <- setNames(object = df_choices$choice_label, nm = df_choices$choice_code)
+
+
+# dap
+dap <- read_csv("inputs/r_dap_ipe_sev_verification.csv")
+df_ref_pop <- read_csv("inputs/refugee_population_ipe.csv")
+
+# make composite indicator ------------------------------------------------
+
+df_with_composites <- df_main_clean_data |>  
+  mutate(strata = paste0(settlement, "_refugee"))
+
+# create weights ----------------------------------------------------------
+
+# refugee weights
+ref_weight_table <- make_refugee_weight_table(input_df_ref = df_with_composites, 
+                                              input_refugee_pop = df_ref_pop)
+df_ref_with_weights <- df_with_composites |>  
+  left_join(ref_weight_table, by = "strata")
+
+
+# set up design object ----------------------------------------------------
+
+ 
+ref_svy <- as_survey(.data = df_ref_with_weights, strata = strata, weights = weights)
+
+
+# analysis ----------------------------------------------------------------
+
+df_main_analysis <- analysis_after_survey_creation(input_svy_obj = ref_svy,
+                                                   input_dap = dap)
+# merge analysis
+
+combined_analysis <- df_main_analysis
+
+# add labels
+full_analysis_labels <- combined_analysis |> 
+  mutate(variable = ifelse(is.na(variable) | variable %in% c(""), variable_val, variable),
+         select_type = "select_one") |> 
+  mutate(variable_val_label = recode(variable_val, !!!qn_label_lookup))
+
+# convert to percentage
+full_analysis_long <- full_analysis_labels |> 
+  mutate(`mean/pct` = ifelse(select_type %in% c("integer") & !str_detect(string = variable, pattern = "^i\\."), `mean/pct`, `mean/pct`*100),
+         `mean/pct` = round(`mean/pct`, digits = 2)) |> 
+  select(`Question`= variable, 
+         variable, 
+         `choices/options` = variable_val, 
+         `choices/options label` = variable_val_label, 
+         `Results(mean/percentage)` = `mean/pct`, 
+         n_unweighted, 
+         population, 
+         subset_1_name, 
+         subset_1_val)
+
+# output analysis
+write_csv(full_analysis_long, paste0("outputs/", butteR::date_file_prefix(), "_full_analysis_lf_ipe_verification_sev.csv"), na="")
