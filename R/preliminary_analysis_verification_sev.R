41082e12872f08e023c82adfaa569223f194588d
Anthony Twesigye <twesigye10@gmail.com>
2023-05-08 21:14:44 +0300

    rename columns

--- R/preliminary_analysis_verification_sev.R
+++ R/preliminary_analysis_verification_sev.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(srvyr)
 library(supporteR)
 
-# source("R/composite_indicators.R")
+source("R/composite_indicators.R")
 source("R/make_weights.R")
 
 # clean data
@@ -10,8 +10,16 @@ data_path <- "inputs/REACH DataPWD/combined_ipe_verif_data_with_batch_name.csv"
 
 df_main_clean_data <- readr::read_csv(file =  data_path)
 
-# # question/choices codes and labels
-df_choices <- readxl::read_excel("inputs/REACH DataPWD/Questions and Responses CODES.xlsx", sheet = "original") |> 
+# question/choices codes and labels
+df_questions <- readxl::read_excel("inputs/REACH DataPWD/Questions and Responses CODES.xlsx", sheet = "Sheet1") |> 
+  filter(!is.na(Question)) |> 
+  mutate(question_code = as.character(QuestionID),
+         question_label = as.character(Question),
+         question_name = as.character(name)
+  ) |> 
+  select(question_code, question_label, question_name)
+
+df_choices <- readxl::read_excel("inputs/REACH DataPWD/Questions and Responses CODES.xlsx", sheet = "Sheet1") |> 
   mutate(choice_code = as.character(AnswerID),
          choice_label = as.character(Answer)) |> 
   select(choice_code, choice_label)
@@ -25,7 +33,9 @@ df_ref_pop <- read_csv("inputs/refugee_population_ipe.csv")
 
 # make composite indicator ------------------------------------------------
 
-df_with_composites <- df_main_clean_data |>  
+df_with_composites <- df_main_clean_data |> 
+  rename(any_of(setNames(df_questions$question_code, df_questions$question_name))) |> 
+  create_composites_verification_sev() |>  
   mutate(strata = paste0(settlement, "_refugee"))
 
 # create weights ----------------------------------------------------------
