ccba2ef2f1aa3498c196feca7b9c83e855fc8214
Anthony Twesigye <twesigye10@gmail.com>
2023-05-19 20:48:31 +0300

    checking updated data

--- R/match_sampled_and_verification_data.R
+++ R/match_sampled_and_verification_data.R
@@ -11,8 +11,7 @@ colnames(df_individual_data_prog)
 
 df_data_delim <- readr::read_delim(file = ind_data_loc, delim = ";")
 colnames(df_data_delim)
-
-
+# verification adjumani
 ipe_data_adjumani <- "inputs/REACH DataPWD/Adjumani IPE 06042023.csv"
 df_ipe_data_adjumani <- readr::read_delim(file = ipe_data_adjumani, delim = ";")
 colnames(df_ipe_data_adjumani)
@@ -20,11 +19,11 @@ colnames(df_ipe_data_adjumani)
 data_path_verification <- "outputs/combined_ipe_verif_data_with_batch_name.csv"
 
 df_ipe_data_complete_verification <- readr::read_csv(file =  data_path_verification)
-
+colnames(df_ipe_data_complete_verification)
   
 # sampled data ------------------------------------------------------------
 
-sampled_data_loc <- "inputs/Reach Household visit/IPE_questionnaire_for_sampled_households_sqlite.xlsx"
+sampled_data_loc <- "inputs/Reach Household visit/IPE_questionnaire_for_sampled_households_sqlite_2023_05_12.xlsx"
 
 # data_nms_sample <- names(readxl::read_excel(path = sampled_data_loc, n_max = 2000))
 # c_types_sample <- ifelse(str_detect(string = data_nms_sample, pattern = "^col1$|^col2$|^col3$"), "date", "guess")
@@ -45,10 +44,12 @@ df_hh_sampled_data <- readxl::read_excel(sampled_data_loc)
 
 # sampled data matching
 
-df_id_mappings <- readr::read_csv("inputs/REACH DataPWD/RegistrationGroupDataMappingKeys.csv")
-
+df_id_mappings <- readr::read_csv("inputs/REACH DataPWD/Registration_Mapping_allTablesFinal_shared_2023_05_12.csv")
+colnames(df_id_mappings)
+# "GroupAnonymizedIPEHHVisit" "IndividualproGresData"     "AnonymizeGroupProGresData"
+# "GroupAnonymizedIPEIndiv"   "IndivAnonymizedIPEIndiv"
 df_sampled_matching <- df_hh_sampled_data |> 
-  filter(GroupAnonymized %in% df_id_mappings$GroupAnonymized)
+  filter(GroupAnonymized %in% df_id_mappings$GroupAnonymizedIPEHHVisit)
 
 # nrow(df_hh_sampled_data) ##: 23835
 
@@ -57,12 +58,15 @@ nrow(df_sampled_matching) ## : 16115
 # deficit: 7720
 # deficit
 df_sampled_not_matching <- df_hh_sampled_data |> 
-  filter(!GroupAnonymized %in% df_id_mappings$GroupAnonymized)
+  filter(!GroupAnonymized %in% df_id_mappings$GroupAnonymizedIPEHHVisit) # GroupAnonymizedIPEHHVisit
 nrow(df_sampled_not_matching)
-df_sampled_not_matching |> select(GroupAnonymized) |> 
+df_sampled_not_matching |> select(GroupAnonymized, settlement) |> 
   write_csv("outputs/un_matching_GroupAnonymized_sampled_data.csv")
 
 
 # verification data matching
 df_verification_matching <- df_ipe_data_complete_verification |> 
-  filter(AnonymizedGrp %in% df_id_mappings$AnonymizedGrp)
+  filter(progres_individualid %in% df_id_mappings$IndividualproGresData) # IndividualproGresData
+# not matching verification data
+df_verification_not_matching <- df_ipe_data_complete_verification |> 
+  filter(!progres_individualid %in% df_id_mappings$IndividualproGresData) # IndividualproGresData
