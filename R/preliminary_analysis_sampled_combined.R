ccc0b9c02a7771683bca19144cbe243ad3b4a1b3
Anthony Twesigye <twesigye10@gmail.com>
2023-09-21 09:40:01 +0300

    handle indicators without data

--- R/preliminary_analysis_sampled_combined.R
+++ R/preliminary_analysis_sampled_combined.R
@@ -16,9 +16,9 @@ df_main_clean_data <- readxl::read_excel(path = data_path, sheet = "cleaned_data
 # tool
 df_survey <- readxl::read_excel("inputs/Individual_Profiling_Exercise_Tool.xlsx", sheet = "survey") 
 
-df_tool_data_support <- df_survey |> 
-  select(type, name, label) |> 
-  filter(str_detect(string = type, pattern = "integer|date|select_one|select_multiple")) |> 
+df_tool_data_support <- df_survey %>% 
+  select(type, name, label) %>% 
+  filter(str_detect(string = type, pattern = "integer|date|select_one|select_multiple")) %>% 
   separate(col = type, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop" )
 
 # dap
@@ -27,7 +27,7 @@ df_ref_pop <- read_csv("inputs/refugee_population_ipe.csv")
 
 # make composite indicator ------------------------------------------------
 
-df_with_composites <- create_composites_sampled(input_df = df_main_clean_data) |>  
+df_with_composites <- create_composites_sampled(input_df = df_main_clean_data) %>%  
   mutate(strata = paste0(settlement, "_refugee"))
 
 # create weights ----------------------------------------------------------
@@ -35,7 +35,7 @@ df_with_composites <- create_composites_sampled(input_df = df_main_clean_data) |
 # refugee weights
 ref_weight_table <- make_refugee_weight_table(input_df_ref = df_with_composites, 
                                               input_refugee_pop = df_ref_pop)
-df_ref_with_weights <- df_with_composites |>  
+df_ref_with_weights <- df_with_composites %>%  
   left_join(ref_weight_table, by = "strata")
 
 
@@ -48,17 +48,20 @@ ref_svy <- as_survey(.data = df_ref_with_weights, strata = strata, weights = wei
 # analysis ----------------------------------------------------------------
 
 df_main_analysis <- analysis_after_survey_creation(input_svy_obj = ref_svy,
-                                                   input_dap = dap)
+                                                   input_dap = dap %>% filter(!variable %in% c("access_to_agriculture_plot_size",
+                                                                                              "qty_bananas_including_matoke",
+                                                                                              "proportion_consumed",
+                                                                                              "how_much_earned")))
 # merge analysis
 
 combined_analysis <- df_main_analysis
 
 # add labels
-full_analysis_labels <- combined_analysis |> 
+full_analysis_labels <- combined_analysis %>%  
   mutate(variable = ifelse(is.na(variable) | variable %in% c(""), variable_val, variable),
-         int.variable = variable) |> 
-  left_join(df_tool_data_support, by = c("int.variable" = "name")) |> 
-  relocate(label, .after = variable) |> 
+         int.variable = variable) %>% 
+  left_join(df_tool_data_support, by = c("int.variable" = "name")) %>% 
+  relocate(label, .after = variable) %>% 
   mutate(select_type = case_when(int.variable %in% c("children_not_attending") ~ "integer",
                                  int.variable %in% c("travel_time_primary", 
                                                      "travel_time_secondary",
@@ -66,10 +69,10 @@ full_analysis_labels <- combined_analysis |>
                                  TRUE ~ select_type))
 
 # convert to percentage
-full_analysis_long <- full_analysis_labels |> 
+full_analysis_long <- full_analysis_labels %>% 
   mutate(label = ifelse(is.na(label), variable, label),
          `mean/pct` = ifelse(select_type %in% c("integer") & !str_detect(string = variable, pattern = "^i\\."), `mean/pct`, `mean/pct`*100),
-         `mean/pct` = round(`mean/pct`, digits = 2)) |> 
+         `mean/pct` = round(`mean/pct`, digits = 2)) %>% 
   select(`Question`= label, 
          variable, 
          `choices/options` = variable_val, 
@@ -77,7 +80,8 @@ full_analysis_long <- full_analysis_labels |>
          n_unweighted, 
          population, 
          subset_1_name, 
-         subset_1_val)
+         subset_1_val) %>% 
+  mutate(dataset = "IPE sampled data")
 
 # output analysis
 write_csv(full_analysis_long, paste0("outputs/", butteR::date_file_prefix(), "_full_analysis_lf_ipe_hh_sampled.csv"), na="")
