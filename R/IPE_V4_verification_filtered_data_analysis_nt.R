3ba1ac18f04a1b2a22d7290fa93699f0f6d4b79d
Anthony Twesigye <twesigye10@gmail.com>
2024-01-23 19:31:15 +0300

    modified analysis

--- R/IPE_V4_verification_filtered_data_analysis_nt.R
+++ R/IPE_V4_verification_filtered_data_analysis_nt.R
@@ -2,8 +2,6 @@ library(tidyverse)
 library(srvyr)
 library(supporteR)
 
-source("R/make_weights.R")
-
 
 # question/choices codes and labels
 df_questions <- readxl::read_excel("inputs/REACH DataPWD/Questions and Responses CODES.xlsx", sheet = "Sheet1") %>% 
@@ -22,10 +20,9 @@ df_choices <- readxl::read_excel("inputs/REACH DataPWD/Questions and Responses C
 choice_label_lookup <- setNames(object = df_choices$choice_label, nm = df_choices$choice_code)
 
 # dap
-dap_verification <- read_csv("inputs/r_dap_ipe_verification_nt.csv")
+dap_verification_hh <- read_csv("inputs/r_dap_ipe_verification_filtered_hh.csv")
+dap_verification_individual <- read_csv("inputs/r_dap_ipe_verification_filtered_ind.csv")
 
-df_questions_dap <- df_questions %>% 
-  filter(question_name %in% dap_verification$variable)
 
 # verif filtered data
 verif_filtered_data_path <- "inputs/ipe_verif_filtered_data_with_composites.xlsx"
@@ -34,8 +31,7 @@ verif_filtered_data_nms <- names(readxl::read_excel(path = verif_filtered_data_p
 verif_filtered_c_types <- ifelse(str_detect(string = verif_filtered_data_nms, pattern = "i.dependency_ratio"), "numeric", "text")
 df_verif_hh_data <- readxl::read_excel(path = verif_filtered_data_path, 
                                        sheet = "verif_hh_data", 
-                                       col_types = verif_filtered_c_types, na = "NA") %>% 
-  mutate(strata = paste0(settlement, "_refugee"))
+                                       col_types = verif_filtered_c_types, na = "NA")
 
 # verification individual data
 verif_ind_data_nms <- names(readxl::read_excel(path = verif_filtered_data_path, n_max = 2000, sheet = "verif_individual_data"))
@@ -43,95 +39,70 @@ verif_ind_c_types <- ifelse(str_detect(string = verif_ind_data_nms, pattern = "p
 df_verif_individual_data <- readxl::read_excel(path = verif_filtered_data_path, 
                                                sheet = "verif_individual_data", 
                                                col_types = verif_ind_c_types, na = "NA") %>% 
-  mutate(strata = paste0(settlement, "_refugee"))
+  mutate(across(.cols = starts_with("i."), .fns = ~as.character(.x)))
 
-df_combined_verification_data <- readr::read_csv(file =  data_path, na = "NULL") %>% 
-  filter(AnonymizedGrp %in% df_hh_data$anonymizedgroup) %>% 
-  rename(any_of(setNames(df_questions_dap$question_code, df_questions_dap$question_name)))  %>%  
-  mutate(across(.cols = any_of(df_questions_dap$question_name), .fns = ~as.character(.x)))
-
-# population figures
-df_ref_pop <- read_csv("inputs/refugee_population_ipe.csv")
-
-
-# separate hh analysis and individual analysis ----------------------------
-
-
-
-
-# make composite indicator ------------------------------------------------
-
-df_with_composites_verification <- df_combined_verification_data %>% 
-  create_composites_verification() %>%  
-  mutate(settlement = progres_coalocationlevel2name,
-         strata = paste0(settlement, "_refugee"))
 
 # create weights ----------------------------------------------------------
-
-# refugee weights
-ref_weight_table <- make_refugee_weight_table(input_df_ref = df_hh_data, 
-                                              input_refugee_pop = df_ref_pop)
-
-df_ref_with_weights_verification <- df_with_composites_verification %>%  
-  left_join(ref_weight_table, by = "strata")
+# refugee weights table
+ref_weight_table <- read_csv("inputs/ipe_weights_table.csv")
 
 
-# set up design object ----------------------------------------------------
+# verif hh level analysis -------------------------------------------------
 
- 
-ref_svy_verification <- as_survey(.data = df_ref_with_weights_verification, strata = strata, weights = weights)
+df_verif_hh_data_with_weights <- df_verif_hh_data %>%  
+  left_join(ref_weight_table, by = "strata") %>% 
+  filter(!is.na(weights))
+
+hh_svy_verification <- as_survey(.data = df_verif_hh_data_with_weights, strata = strata, weights = weights)
+
+df_hh_level_analysis_verif <- analysistools::create_analysis(design = hh_svy_verification, loa = dap_verification_hh, sm_separator = "/")
 
 
-# analysis ----------------------------------------------------------------
+# verif individual level analysis -----------------------------------------
 
-# main data
-df_main_analysis_verification <- analysis_after_survey_creation(input_svy_obj = ref_svy_verification,
-                                                   input_dap = dap_verification) %>% 
-  mutate(level = "Individual")
+df_verif_individual_data_with_weights <- df_verif_individual_data %>% 
+  left_join(ref_weight_table, by = "strata") %>% 
+  filter(!is.na(weights))
 
-# protection analysis // filter on age
-ref_svy_prot <- as_survey(.data = df_ref_with_weights_verification %>% filter(progres_age > 4, progres_age < 18), 
-                          strata = strata, weights = weights)
-df_prot_analysis <- analysis_after_survey_creation(input_svy_obj = ref_svy_prot,
-                                                   input_dap = dap_verification %>% filter(variable %in% c("avg_time_child_working_payment", 
-                                                                                              "child_work_involve",
-                                                                                              "children_5_17_years_working_to_support_hh_for_payment",
-                                                                                              "children_engaged_child_labour"))) %>% 
-  mutate(level = "Individual")
+ind_svy_verification <- as_survey(.data = df_verif_individual_data_with_weights, strata = strata, weights = weights)
 
-# merge analysis
+df_ind_level_analysis_verif <- analysistools::create_analysis(design = ind_svy_verification, loa = dap_verification_individual, sm_separator = "/")
 
-combined_analysis_verification <- bind_rows(df_main_analysis_verification, df_prot_analysis)
-
-# add labels
-full_analysis_labels_verification <- combined_analysis_verification %>% 
-  mutate(variable = ifelse(is.na(variable) | variable %in% c(""), variable_val, variable),
-         select_type = "select_one") %>% 
-  mutate(variable_code = recode(variable, !!!setNames(df_questions_dap$question_code, df_questions_dap$question_name)),
-         variable_label = recode(variable, !!!setNames(df_questions_dap$question_label, df_questions_dap$question_name)),
-         variable_val_label = recode(variable_val, !!!choice_label_lookup))
-
-# convert to percentage
-full_analysis_long_verification <- full_analysis_labels_verification %>% 
-  mutate(`mean/pct` = ifelse(select_type %in% c("integer") & !str_detect(string = variable, pattern = "^i\\."), `mean/pct`, `mean/pct`*100),
-         `mean/pct` = round(`mean/pct`, digits = 2)) %>% 
-  select(`Question code`= variable_code, 
-         `Question`= variable,
-         `Question label`= variable_label,
-         variable, 
-         `choices/options` = variable_val, 
-         `choices/options label` = variable_val_label, 
-         `Results(mean/percentage)` = `mean/pct`, 
-         n_unweighted, 
-         population, 
-         subset_1_name, 
-         subset_1_val,
-         select_type,
-         level) %>% 
-  mutate(dataset = "IPE verification data")
+# # merge analysis
+# 
+# combined_analysis_verification <- bind_rows(df_main_analysis_verification, df_prot_analysis)
+# 
+# # add labels
+# full_analysis_labels_verification <- combined_analysis_verification %>% 
+#   mutate(variable = ifelse(is.na(variable) | variable %in% c(""), variable_val, variable),
+#          select_type = "select_one") %>% 
+#   mutate(variable_code = recode(variable, !!!setNames(df_questions_dap$question_code, df_questions_dap$question_name)),
+#          variable_label = recode(variable, !!!setNames(df_questions_dap$question_label, df_questions_dap$question_name)),
+#          variable_val_label = recode(variable_val, !!!choice_label_lookup))
+# 
+# # convert to percentage
+# full_analysis_long_verification <- full_analysis_labels_verification %>% 
+#   mutate(`mean/pct` = ifelse(select_type %in% c("integer") & !str_detect(string = variable, pattern = "^i\\."), `mean/pct`, `mean/pct`*100),
+#          `mean/pct` = round(`mean/pct`, digits = 2)) %>% 
+#   select(`Question code`= variable_code, 
+#          `Question`= variable,
+#          `Question label`= variable_label,
+#          variable, 
+#          `choices/options` = variable_val, 
+#          `choices/options label` = variable_val_label, 
+#          `Results(mean/percentage)` = `mean/pct`, 
+#          n_unweighted, 
+#          population, 
+#          subset_1_name, 
+#          subset_1_val,
+#          select_type,
+#          level) %>% 
+#   mutate(dataset = "IPE verification data")
 
 # output analysis
-write_csv(full_analysis_long_verification %>% select(-select_type), 
-          paste0("outputs/", butteR::date_file_prefix(), "_full_analysis_lf_ipe_verification.csv"), na="")
-write_csv(full_analysis_long_verification, paste0("outputs/full_analysis_lf_ipe_verification.csv"), na="")
+analysis_out_list <- list("HH level analysis" = df_hh_level_analysis_verif$results_table,
+                          "Individual level analysis" = df_ind_level_analysis_verif$results_table)
+
+openxlsx::write.xlsx(analysis_out_list, paste0("outputs/", butteR::date_file_prefix(), "_analysis_ipe_verification_filtered.xlsx"))
+openxlsx::write.xlsx(analysis_out_list, paste0("outputs/analysis_ipe_verification_filtered.xlsx"))
 
