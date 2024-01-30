b8ef878e30b91cf1cfd6b2fcb6a32960aab744f9
Anthony Twesigye <twesigye10@gmail.com>
2024-01-30 16:49:57 +0300

    changed pipe and restoring 1 0 for select multiple

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -10,18 +10,18 @@ log_loc <- "inputs/combined_checks_IPE_questionnaire_for_sampled_households.xlsx
 
 log_col_types <- ifelse(str_detect(string = names(readxl::read_excel(path = log_loc, n_max = 5000)), pattern = "start_date|checked_date"), "date",  "text")
 
-df_cleaning_log <- readxl::read_excel(log_loc, col_types = log_col_types) |> 
-  filter(!adjust_log %in% c("delete_log"), reviewed %in% c("1")) |>
+df_cleaning_log <- readxl::read_excel(log_loc, col_types = log_col_types) %>% 
+  filter(!adjust_log %in% c("delete_log"), reviewed %in% c("1")) %>%
   mutate(adjust_log = ifelse(is.na(adjust_log), "apply_suggested_change", adjust_log),
          value = ifelse(is.na(value) & str_detect(string = issue_id, pattern = "logic_c_"), "blank", value),
          value = ifelse(type %in% c("remove_survey"), "blank", value),
          name = ifelse(is.na(name) & type %in% c("remove_survey"), "point_number", name)
-  ) |> 
-  filter(!is.na(value), !is.na(uuid)) |>
+  ) %>% 
+  filter(!is.na(value), !is.na(uuid)) %>%
   mutate(value = ifelse(value %in% c("blank"), NA, value),
          # sheet = NA,
          # index = NA,
-         relevant = NA) |>
+         relevant = NA) %>%
   select(uuid, type, name, value, issue_id, sheet, index, relevant, issue)
 
 
@@ -34,9 +34,9 @@ cols_to_escape <- c("index", "start", "end", "today", "starttime",	"endtime", "_
 data_nms <- names(readxl::read_excel(path = loc_data, sheet = "main_data", n_max = 5000))
 c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "guess")
 
-df_raw_data <- readxl::read_excel(path = loc_data, sheet = "main_data", col_types = c_types) |> 
-  group_by(`_uuid`) |> 
-  mutate( `_uuid` = ifelse(row_number() > 1, paste0(`_uuid`, "2"), `_uuid`)) |> 
+df_raw_data <- readxl::read_excel(path = loc_data, sheet = "main_data", col_types = c_types) %>% 
+  group_by(`_uuid`) %>% 
+  mutate( `_uuid` = ifelse(row_number() > 1, paste0(`_uuid`, "2"), `_uuid`)) %>% 
   ungroup()
 
 # loop
@@ -59,7 +59,7 @@ df_choices <- readxl::read_excel(loc_tool, sheet = "choices")
 
 # main dataset ------------------------------------------------------------
 
-df_cleaning_log_main <-  df_cleaning_log |> 
+df_cleaning_log_main <-  df_cleaning_log %>% 
   filter(is.na(sheet))
 rm(df_cleaning_log)
 df_cleaning_step <- supporteR::cleaning_support(input_df_raw_data = df_raw_data,
@@ -67,7 +67,7 @@ df_cleaning_step <- supporteR::cleaning_support(input_df_raw_data = df_raw_data,
                                                input_df_choices = df_choices,
                                                input_df_cleaning_log = df_cleaning_log_main) 
 
-df_cleaned_data <- df_cleaning_step |> 
+df_cleaned_data <- df_cleaning_step %>% 
   mutate(bucket_18l_num = 18*bucket_18l_num,
          bucket_15l_num = 15*bucket_15l_num,
          bucket_10l_num = 10*bucket_10l_num,
@@ -75,14 +75,16 @@ df_cleaned_data <- df_cleaning_step |>
          jerrycan_10l_num = 10*jerrycan_10l_num,
          jerrycan_5l_num = 5*jerrycan_5l_num,
          jerrycan_3l_num = 3*jerrycan_3l_num,
-         ) |> 
-  rowwise() |> 
+         ) %>% 
+  rowwise() %>% 
   mutate(calc_monthly_expenditure = sum(c_across(exp_savings:exp_sanitary_materials), na.rm = TRUE),
         int.calc_total_volume = sum(c_across(bucket_18l_num:jerrycan_3l_num), na.rm = TRUE),
-         ) |> 
-  ungroup() |> 
+         ) %>% 
+  ungroup() %>% 
   mutate(calc_total_volume = int.calc_total_volume * number_of_trips_for_each_container,
-         calc_total_volume_per_person = (calc_total_volume/(hh_size*number_of_days_collected_water_lasts))) |> 
+         calc_total_volume_per_person = (calc_total_volume/(hh_size*number_of_days_collected_water_lasts))) %>% 
+  mutate(across(contains("/"), .fns = ~ case_when(.x %in% c(TRUE) ~ "1",
+                                                  .x %in% c(FALSE) ~ "0"))) %>% 
   select(-starts_with("int."))
 
 # write final datasets out -----------------------------------------------
