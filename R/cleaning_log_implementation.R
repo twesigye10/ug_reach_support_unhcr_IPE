7d3d4a0e7ddf53de4ba8b07f26578228878f6d3d
Anthony Twesigye <twesigye10@gmail.com>
2024-01-30 16:03:55 +0300

    updating cleaning

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(lubridate)
 library(glue)
 library(supporteR)
 
@@ -32,32 +31,25 @@ loc_data <- "inputs/Individual_Profiling_Exercise_Questionnaire_for_Sampled_Hous
 cols_to_escape <- c("index", "start", "end", "today", "starttime",	"endtime", "_submission_time", "_submission__submission_time",
                     "date_last_received")
 
-data_nms <- names(readxl::read_excel(path = loc_data, n_max = 5000))
+data_nms <- names(readxl::read_excel(path = loc_data, sheet = "main_data", n_max = 5000))
 c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "guess")
 
-df_raw_data <- readxl::read_excel(path = loc_data, col_types = c_types) |> 
-  mutate(across(.cols = -c(contains(cols_to_escape)), 
-                .fns = ~ifelse(str_detect(string = ., 
-                                          pattern = fixed(pattern = "N/A", ignore_case = TRUE)), "NA", .))) |> 
+df_raw_data <- readxl::read_excel(path = loc_data, sheet = "main_data", col_types = c_types) |> 
   group_by(`_uuid`) |> 
-  filter(row_number() == 1) |> 
+  mutate( `_uuid` = ifelse(row_number() > 1, paste0(`_uuid`, "2"), `_uuid`)) |> 
   ungroup()
 
-# final shared hh data
-sampled_data_loc3 <- "inputs/Individual_Profiling_Exercise_Questionnaire_for_Sampled_Households_20230927.xlsx"
-df_sampled_data3 <- readxl::read_excel(sampled_data_loc3)
-
 # loop
-loc_loop_data <- "inputs/repeat_mental_health.xlsx"
-
-data_nms_loop <- names(readxl::read_excel(path = loc_loop_data, n_max = 5000))
+data_nms_loop <- names(readxl::read_excel(path = loc_data, sheet = "mental_health", n_max = 5000))
 c_types_loop <- ifelse(str_detect(string = data_nms_loop, pattern = "^feel_so_|help_from_mhpss_worker|often_unable_to_carry_out_essential_activities_due_to_feelings"), "text", "guess")
 
-loop_data <- readxl::read_excel(loc_loop_data, col_types = c_types_loop)
+loop_data <- readxl::read_excel(loc_data, sheet = "mental_health", col_types = c_types_loop)
 
-df_loop_mental_health <- loop_data %>% 
-  # filter(consent_mental_health %in% c("yes"),  !is.na(feel_so_afraid))
-  filter(consent_mental_health %in% c("yes"),  if_all(feel_so_afraid:feel_so_severely_upset_about_bad_things_that_happened, ~ !is.na(.x)))
+df_log_remove_loop_entry <- df_cleaning_log %>% 
+  filter(type %in% c("remove_loop_entry"))
+
+df_loop_mental_health <- loop_data %>%
+  filter(!`_index` %in% df_log_remove_loop_entry$index, `_submission__uuid` %in% df_raw_data$`_uuid`)
 
 # tool
 loc_tool <- "inputs/Individual_Profiling_Exercise_Tool.xlsx"
@@ -76,9 +68,7 @@ df_cleaning_step <- supporteR::cleaning_support(input_df_raw_data = df_raw_data,
                                                input_df_cleaning_log = df_cleaning_log_main) 
 
 df_cleaned_data <- df_cleaning_step |> 
-  mutate(across(.cols = -c(any_of(cols_to_escape), matches("_age$|^age_|uuid")),
-                .fns = ~ifelse(str_detect(string = ., pattern = "^[9]{3,9}$"), "NA", .)),
-         bucket_18l_num = 18*bucket_18l_num,
+  mutate(bucket_18l_num = 18*bucket_18l_num,
          bucket_15l_num = 15*bucket_15l_num,
          bucket_10l_num = 10*bucket_10l_num,
          jerrycan_20l_num = 20*jerrycan_20l_num,
@@ -95,17 +85,10 @@ df_cleaned_data <- df_cleaning_step |>
          calc_total_volume_per_person = (calc_total_volume/(hh_size*number_of_days_collected_water_lasts))) |> 
   select(-starts_with("int."))
 
-# add anonymizedgroup to the original raw data
-df_cleaned_data_updated <- df_cleaned_data %>% 
-  left_join(df_sampled_data3 %>% select(uuid = `_uuid`, anonymizedgroup)) %>% 
-  mutate(GroupAnonymized = anonymizedgroup) %>% 
-  select(-anonymizedgroup) %>% 
-  rename(anonymizedgroup = GroupAnonymized)
-
 # write final datasets out -----------------------------------------------
 
-list_of_clean_datasets <- list("cleaned_data" = df_cleaned_data_updated,
-                               "mental_health" = df_loop_mental_health %>% filter(`_submission__uuid` %in% df_cleaned_data_updated$uuid))
+list_of_clean_datasets <- list("cleaned_data" = df_cleaned_data,
+                               "mental_health" = df_loop_mental_health %>% filter(`_submission__uuid` %in% df_cleaned_data$uuid))
 
 openxlsx::write.xlsx(x = list_of_clean_datasets,
                      file = paste0("outputs/", butteR::date_file_prefix(), 
